3d1d9f5e206d07af4818dd8ed72c69df307dd5a7
clbwth <celebwath@gmail.com>
2016-07-11 10:59:43 +0200

    new area view modified
    added form validation to asst_new.html, fixed newArea function in
    inspectionController

---
+++ www/js/form_ctrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Gabriele on 07/07/2016.
+ */
+angular.module('Astrid_Op')
+  .controller('formCtrl', ['$scope', function ($scope, $ionicPlatform)
+  {
+
+  }]);
--- www/js/isp_ctrl.js
+++ www/js/isp_ctrl.js
@@ -4,16 +4,22 @@
 angular.module('Astrid_Op')
   .controller('inspectionCtrl', ['$scope', function ($scope, $cordovaSQLite, $ionicPlatform)
   {
-    $scope.show_adder = false;
-
-    $scope.toggleState = function ()
+    $scope.newArea = function ()
     {
-      $scope.show_adder == true ? $scope.show_adder = false : $scope.show_adder = true;
+      document.getElementById("newGame").style.visibility = "visible";
+      console.log('ciao');
+      var query = "INSERT INTO table_inspections (ga_name, town_of, is_inspected) VALUES (?,?,?,?)";
+      document.addEventListener("deviceready", function () {
+        AstridDB.executeSql(query, [$scope.area, $scope.comune, 0], function (resultSet) {
+          console.log('resultSet.insertId: ' + resultSet.insertId);
+          console.log('resultSet.rowsAffected: ' + resultSet.rowsAffected);
+          $scope.show_adder == true ? $scope.show_adder = false : $scope.show_adder = true;
+        }, function (error) {
+          console.log('SELECT error: ' + error.message);
+        });
+      });
+
     };
 
-    $scope.new_area = function ()
-    {
-      
-    }
   }]);
 
